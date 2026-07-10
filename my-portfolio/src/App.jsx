ac3947037cd572573ab2a8a8cddc38e9ad6955f4
Mayank Prakash <tomayankprakash@gmail.com>
2026-07-10 14:09:35 +0530

    initial commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Aditya Prakash</h1>
+      <h2>Java Backend Developer</h2>
+      <p>Java | Spring Boot | AWS | MySQL</p>
+    </div>
+  );
+}
+
+export default App;
